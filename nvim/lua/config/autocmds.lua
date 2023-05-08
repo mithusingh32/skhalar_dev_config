d86183e6cb5f34a3e878dcf692356f590d0ae704
Sukhmeet Khalar <sukhmeet@sukhmeetkhalar.com>
2023-05-08 10:27:57 -0400

    Tabbing for cmp  does not go to previous line.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,15 +2,24 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- This should fix the tab going back to the initial insert (autocomplete window)
+-- https://github.com/L3MON4D3/LuaSnip/issues/656
+local luasnip = require("luasnip")
 vim.api.nvim_create_autocmd("ModeChanged", {
-  pattern = "*",
-  callback = function()
-    if
-      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
-      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-      and not require("luasnip").session.jump_active
-    then
-      require("luasnip").unlink_current()
+  group = vim.api.nvim_create_augroup("UnlinkLuaSnipSnippetOnModeChange", {
+    clear = true,
+  }),
+  pattern = { "s:n", "i:*" },
+  desc = "Forget the current snippet when leaving the insert mode",
+  callback = function(evt)
+    -- If we have n active nodes, n - 1 will still remain after a `unlink_current()` call.
+    -- We unlink all of them by wrapping the calls in a loop.
+    while true do
+      if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+        luasnip.unlink_current()
+      else
+        break
+      end
     end
   end,
 })
