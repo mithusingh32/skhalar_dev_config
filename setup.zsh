a2b4709b5576ed8f988c3a9aacafa863de83d8ea
Sukhmeet Khalar <sukhmeet@sukhmeetkhalar.com>
2023-07-20 10:18:39 -0400

    added setup script

---
+++ setup.zsh
@@ -0,0 +1,41 @@
+#!/bin/zsh 
+
+repo_dir=${0:a:h}
+
+echo $repo_dir 
+
+echo "installing pre-reqs"
+sudo apt install -y fd-find luarocks ripgrep curl
+sudo luarocks install jsregexp
+curl -L git.io/antigen > $HOME/.oh-my-zsh/custom/plugins/antigen.zsh
+chmod +x  $HOME/.oh-my-zsh/custom/plugins/antigen.zsh
+
+echo "Please install required fonts"
+# TODO automate download and install of fonsts
+
+echo "Setting up LazyVim"
+ln -sf $repo_dir/nvim $HOME/.config/nvim
+
+
+echo "Setting up zsh"
+if [  -f $HOME/.zshrc ]; then
+	mv -v $HOME/.zshrc $HOME/.zshrc.bak
+fi
+
+if [ -f $HOME/.p10k.zsh ]; then 
+	mv -v $HOME/.p10k.zsh $HOME/.p10k.zsh.bak
+fi
+
+ln -sf $repo_dir/oh-my-zsh/zshrc  $HOME/.zshrc
+ln -sf  $repo_dir/oh-my-zsh/p10k.zsh $HOME/.p10k.zsh
+
+echo "Installing lazy git"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+echo "Installing Jetbrain Toolbox"
+wget -cO jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
+sudo mkdir -p /opt/jetbrains-toolbox
+sudo tar -xzf jetbrains-toolbox.tar.gz -C /opt/jetbrains-toolbox
